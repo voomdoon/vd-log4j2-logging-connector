4256ab2776fcc237bf5faaa8dfed824b31bb14c1
voomdoon <nilreb24-github20141105@yahoo.de>
2022-08-27 22:44:58 +0200

    test: add test for initialization by vd-logging

---
+++ src/test/java/de/voomdoon/logging/connector/log4j2/Initialization_Test.java
@@ -0,0 +1,196 @@
+package de.voomdoon.logging.connector.log4j2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.ErrorHandler;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.DefaultErrorHandler;
+import org.apache.logging.log4j.core.config.AbstractConfiguration;
+import org.apache.logging.log4j.core.config.AppenderRef;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.message.Message;
+import org.junit.jupiter.api.Test;
+
+import de.voomdoon.logging.LogManager;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since DOCME add inception version number
+ */
+class Initialization_Test {
+
+	/**
+	 * {@link Appender} for testing.
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	public static class TestAppender implements Appender {
+
+		/**
+		 * @since 0.1.0
+		 */
+		private List<LogEvent> logEvents;
+
+		/**
+		 * @since 0.1.0
+		 */
+		private String name;
+
+		/**
+		 * @since 0.1.0
+		 */
+		private boolean started;
+
+		/**
+		 * @since 0.1.0
+		 */
+		public TestAppender() {
+			logEvents = new LinkedList<>();
+			name = "jUnit" + System.nanoTime();
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public void append(LogEvent logEvent) {
+			logEvents.add(logEvent);
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public ErrorHandler getHandler() {
+			return new DefaultErrorHandler(this);
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public Layout<? extends Serializable> getLayout() {
+			throw new UnsupportedOperationException("'getLayout' not implemented at 'TestAppender'!");
+		}
+
+		/**
+		 * @return {@link List} of {@link LogEvent}.
+		 * @since 0.1.0
+		 */
+		public List<LogEvent> getLogEvents() {
+			return logEvents;
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public State getState() {
+			throw new UnsupportedOperationException("'getState' not implemented at 'TestAppender'!");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public boolean ignoreExceptions() {
+			return false;
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public void initialize() {
+			throw new UnsupportedOperationException("'initialize' not implemented at 'TestAppender'!");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public boolean isStarted() {
+			return started;
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public boolean isStopped() {
+			throw new UnsupportedOperationException("'isStopped' not implemented at Test_Initialization!");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public void setHandler(ErrorHandler arg0) {
+			throw new UnsupportedOperationException("'setHandler' not implemented at Test_Initialization!");
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public void start() {
+			started = true;
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		public void stop() {
+			started = false;
+		}
+	}
+
+	@Test
+	void test() throws Exception {
+		TestAppender appender = new TestAppender();
+		addAppender(appender);
+
+		LogManager.getLogger(getClass()).info("test-message");
+
+		assertThat(appender.logEvents).element(0).extracting(LogEvent::getMessage)
+				.extracting(Message::getFormattedMessage).isEqualTo("test-message");
+	}
+
+	private void addAppender(TestAppender appender) {
+		LoggerContext context = (LoggerContext) org.apache.logging.log4j.LogManager.getContext(false);
+		AbstractConfiguration config = (AbstractConfiguration) context.getConfiguration();
+		config.addAppender(appender);
+
+		AppenderRef[] refs = new AppenderRef[] { AppenderRef.createAppenderRef(appender.getName(), null, null) };
+
+		LoggerConfig loggerConfig = LoggerConfig.createLogger(true, Level.ALL,
+				org.apache.logging.log4j.LogManager.ROOT_LOGGER_NAME, null, refs, null, config, null);
+
+		appender.start();
+		loggerConfig.addAppender(appender, null, null);
+		config.addLogger(org.apache.logging.log4j.LogManager.ROOT_LOGGER_NAME, loggerConfig);
+		context.updateLoggers();
+	}
+}
